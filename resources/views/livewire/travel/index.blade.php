ed98e3b61d1f2b942bd97639b84e44cf8e16cf79
Eric L <eric@crewcall.com>
2025-11-17 15:55:37 -0500

    Make travel and hotels pages full width

--- resources/views/livewire/travel/index.blade.php
+++ resources/views/livewire/travel/index.blade.php
@@ -1,5 +1,5 @@
 <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="mx-auto sm:px-6 lg:px-8">
         <!-- Header -->
         <div class="mb-6 flex justify-between items-center">
             <div>
